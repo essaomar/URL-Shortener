9ce52e4d0961e7a53ed52165b8393d291046754f
essaomar <creativevalleyone@gmail.com>
2015-04-16 23:44:54 +0800

    started backend coding

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+ 
+$hostname = "localhost";  
+$database = "urldb";  
+$username = "root"; 
+$password = "";  
+//
+$site = mysql_connect($hostname, $username, $password); 
+mysql_select_db($database, $site);
+//
+
+ 
+
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+
+ 
+include('db.php');
+
+ 
+if (!empty($_GET['url'])) {
+	$redirect = mysql_fetch_assoc(mysql_query("SELECT url_link FROM urls WHERE url_short = '".addslashes($_GET['url'])."'"));
+	$redirect = "http://".str_replace("http://","",$redirect[url_link]);
+	header('HTTP/1.1 301 Moved Permanently');  
+	header("Location: ".$redirect);  
+}
+ 
+if ($_POST['url']) {
+
+ 
+$short = substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 5);
+
+mysql_query("INSERT INTO urls (url_link, url_short, url_ip, url_date) VALUES
+
+	(
+	'".addslashes($_POST['url'])."',
+	'".$short."',
+	'".$_SERVER['REMOTE_ADDR']."',
+	'".time()."'
+	)
+
+");
+
+$redirect = "?s=$short";
+header('Location: '.$redirect); die;
+
+}
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>URL shorting</title>
+ 
+</head>
+
+<body>
+
+
+<h1> enter the URL</h1>
+<form id="form1" name="form1" method="post" action="">
+  <input name="url" type="text" id="url" value="" size="75" />
+  <input type="submit" name="Submit" value="submit" />
+</form>
+
+ 
+<?php if (!empty($_GET['s'])) { ?>
+<br />
+<h2>Here's the short URL: <a href="<?php echo $server_name; ?><?php echo $_GET['s']; ?>" target="_blank"><?php echo $server_name; ?><?php echo $_GET['s']; ?></a></h2>
+<?php } ?>
+ 
+ 
+
+</body>
+</html>
